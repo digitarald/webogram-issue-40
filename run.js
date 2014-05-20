488e7123c0965e13fa67a8bef922e99663295b88
Harald Kirschner <hkirschner@mozilla.com>
2014-05-20 15:15:55 -0700

    Responsive clicks

--- run.js
+++ run.js
@@ -8,19 +8,23 @@ var run = {
 
 	start: function() {
 		run.started = Date.now();
+		document.getElementById('started').style.display = 'block';
+		// Flush reflow
+		document.body.offsetHeight;
 	},
 
 	end: function(result) {
+		document.getElementById('started').style.display = 'none';
 		alert('Done: ' + (Date.now() - run.started) / 1000);
 	},
 
 	startMain: function() {
+		run.start();
 		window.setTimeout(function startMainTimeout() {
-			run.start();
 			/* 1dfaf951107f49df - is just sample value */
 			var result = pqPrimeLong(goog.math.Long.fromString('1dfaf951107f49df', 16));
 			run.end(result);
-		});
+		}, 1);
 	},
 
 	startWorker: function() {
